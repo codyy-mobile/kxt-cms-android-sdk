af2409a163b85822ece7ff0e0a66c5b64beacfc3
lijian <lijian@codyy.com>
2019-03-14 16:43:33 +0800

    refactor:移除无用的类文件
    COMMIT END

--- cmssdk/src/main/java/com/codyy/cms/ext/sys/SysStateType.java
+++
@@ -1,9 +0,0 @@
-package com.codyy.cms.ext.sys;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.SOURCE)
-public @interface SysStateType {
-    String IS_KEYBOARD_LOCKED = "is_keyboard_locked";
-}
